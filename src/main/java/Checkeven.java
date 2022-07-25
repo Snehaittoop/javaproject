dc19c2c8c6f49e01ad01e0c3b550e9839fbcaa7d
Snehaittoop <snehaittoop868@gmail.com>
2022-07-25 16:04:18 +0530

    adding

---
+++ src/main/java/Checkeven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Checkeven {
+
+    public static void main(String[] args) {
+            Scanner input=new Scanner(System.in);
+            System.out.println("enter number");12
+            int num = input.nextInt();
+            if(num%2 ==0) {
+                System.out.println(num + "even");
+            }
+            else{
+                System.out.println(num+"odd");
+            }
+
+    }
+}
